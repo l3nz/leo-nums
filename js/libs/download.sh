d748ab4f65c3355af01ffbe5dbc341fb5917b830
lenz <lenz@lenzs-MacBook-Pro.local>
2014-12-29 14:36:37 +0100

    Downlaod of all libs

---
+++ js/libs/download.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+wget https://raw.github.com/ducksboard/gridster.js/master/dist/jquery.gridster.min.js
+wget https://raw.github.com/ducksboard/gridster.js/master/dist/jquery.gridster.min.css
+wget http://code.jquery.com/jquery-1.11.1.js
+
+
